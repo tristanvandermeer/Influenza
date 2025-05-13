6cd89dc5dc646611ae5fd98780d27a0c734cd9f0
RealTribn <tristan.vandermeer@gmail.com>
2025-05-13 14:05:27 +0100

    notes

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import math
 import time
 
 # Say hey what's up hello
+
+""" 
+TO DO :
+
+things
+- HTML smuggling
+- Other C2 Comms
+- Environment Keying and Encryption
+- Shannon Entropy Averaging
+
+overview
+- Shellcode loader
+- Secondary C2
+
+"""
