8d02f5e85effac6b3cbc384d8c57988a073e80fe
RealTribn <tristan.vandermeer@gmail.com>
2025-05-13 14:36:02 +0100

    wowzers

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-import math
-import time
+import base64
 
-# Say hey what's up hello
+# Smuggler
+def htmlify(file_path):
+    with open(file_path, "rb" ) as f:
+        encoded = base64.b64encode(f)
+
+    html = (
+
+    "<!DOCTYPE html"
+    "<html>"
+    "<body>" 
+    f"<!-- insert {encoded} -->"
+    "</body>" 
+    "</html>"
+
+    )
+    
+# Env Key Notes:
+# Stager1 smuggles env key to high command
+# High command encrypts Stager2 with given env key
+# Stager1 recieves encrypted Stager2 and decrypts with env key
+# Is this right?
 
 """ 
 TO DO :
